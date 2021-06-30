3bf4a7dc1337a6aba1372bd9f8865cb3a0aacc34
Herko <herko@index.hu>
2021-06-30 20:29:26 +0200

    Validáció tesztelés

--- locations-spring-solution/src/main/java/locations/UpdateLocationCommand.java
+++ locations-spring-solution/src/main/java/locations/UpdateLocationCommand.java
@@ -1,12 +1,16 @@
 package locations;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateLocationCommand {
 
     @NotBlank(message = "Name cannot be blank")
--- locations-spring-solution/src/test/java/locations/LocationsControllerRestTemplateIT.java
+++ locations-spring-solution/src/test/java/locations/LocationsControllerRestTemplateIT.java
@@ -6,11 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.zalando.problem.Problem;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocationsControllerRestTemplateIT {
@@ -41,6 +45,98 @@ public class LocationsControllerRestTemplateIT {
 
         assertThat(locations)
                 .extracting(LocationDto::getName)
-                .containsExactly("Eindhoven","London");
+                .containsExactly("Eindhoven", "London");
+    }
+
+    @Test
+    void testFindLocationById() {
+        service.deleteAllLocations();
+
+        template.postForObject("/api/locations", new CreateLocationCommand("London", 51.5368, -0.1308), LocationDto.class);
+
+        LocationDto result = template.getForObject("/api/locations/1", LocationDto.class);
+
+        assertEquals(51.5368, result.getLat(), 0.0001);
+
+    }
+
+    @Test
+    void testUpdateLocation() {
+        service.deleteAllLocations();
+
+        template.postForObject("/api/locations", new CreateLocationCommand("London", 51.5368, -0.1308), LocationDto.class);
+
+        template.put("/api/locations/1", new UpdateLocationCommand("Budapest", 70, 150));
+        LocationDto result = template.getForObject("/api/locations/1", LocationDto.class);
+
+        assertEquals(150, result.getLon());
+
+    }
+
+    @Test
+    void testDelete() {
+        service.deleteAllLocations();
+        template.postForObject("/api/locations", new CreateLocationCommand("Eindhoven", 51.4537, 5.4702), LocationDto.class);
+        template.postForObject("/api/locations", new CreateLocationCommand("London", 51.5368, -0.1308), LocationDto.class);
+
+        List<LocationDto> locations = template.exchange("/api/locations",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LocationDto>>() {
+                })
+                .getBody();
+
+        assertThat(locations)
+                .hasSize(2)
+                .extracting(LocationDto::getName)
+                .containsExactly("Eindhoven", "London");
+
+        template.delete("/api/locations/1");
+
+        List<LocationDto> afterDelete = template.exchange("/api/locations",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LocationDto>>() {
+                })
+                .getBody();
+
+        assertThat(afterDelete)
+                .hasSize(1)
+                .extracting(LocationDto::getName)
+                .containsOnly("London");
+    }
+
+    @Test
+    void testValidationWithWrongLat() {
+
+        ResponseEntity<Problem> problem =
+                template.postForEntity("/api/locations", new UpdateLocationCommand("Budapest", 150, 150), Problem.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST,problem.getStatusCode());
+    }
+
+    @Test
+    void testValidationWithWrongLon() {
+
+        ResponseEntity<Problem> problem =
+                template.postForEntity("/api/locations", new UpdateLocationCommand("Budapest", 70, 200), Problem.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST,problem.getStatusCode());
+    }
+
+    @Test
+    void testValidationWithWrongName() {
+
+        ResponseEntity<Problem> problem =
+                template.postForEntity("/api/locations", new UpdateLocationCommand("", 70, 150), Problem.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST,problem.getStatusCode());
+    }
+
+    @Test
+    void test(){
+        ResponseEntity<Problem> problem = template.getForEntity("/api/locations/15",Problem.class);
+
+        assertEquals(HttpStatus.NOT_FOUND,problem.getStatusCode());
     }
 }
